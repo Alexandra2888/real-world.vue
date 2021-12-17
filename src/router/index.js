7a829447cd05b766e24663777121e9214e6c8b7c
Alexandra2888 <moldovan.alexandra28@gmail.com>
2021-12-17 15:22:56 +0200

    adding files

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import About from "../views/About.vue";
+import EventList from "../views/EventList.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "EventList",
+    component: EventList,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
